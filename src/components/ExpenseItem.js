ff8efad643ead80ce5b141c2820979e2848f302f
alirezayoom <alirezayoom@gmail.com>
2023-01-30 00:21:25 +0430

    more JSX code added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,11 @@
 export default function ExpenseItem() {
-  return <div>Expense item</div>;
+  return (
+    <div>
+      <div>Jan 30th 2023</div>
+      <div>
+        <h1>Galaxy s23</h1>
+        <div>$599</div>
+      </div>
+    </div>
+  );
 }
