0978513ab456dd272471c7da1982dec26d731730
alirezayoom <alirezayoom@gmail.com>
2023-01-31 22:36:15 +0430

    style added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 export default function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>Jan 30th 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h1>Galaxy s23</h1>
-        <div>$599</div>
+        <div className="expense-item__price">$599</div>
       </div>
     </div>
   );
