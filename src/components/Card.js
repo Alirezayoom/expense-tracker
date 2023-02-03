e5db07a6885df2273556d85e23ebd6ad59a76ee3
alirezayoom <alirezayoom@gmail.com>
2023-02-03 22:59:46 +0430

    props.className added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import "./Card.css";
 
-export default function Card({ children }) {
-  return <div className="card">{children}</div>;
+export default function Card(props) {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
 }
