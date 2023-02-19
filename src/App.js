c3d758545d1ffdadc064036521078f4097a9c5bd
alirezayoom <alirezayoom@gmail.com>
2023-02-19 08:47:20 +0430

    ExpenseForm added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Expenses from "./components/Expenses/Expenses";
 
+import NewExpense from "./components/NewExpense/NewExpense";
+
 function App() {
   return (
     <div>
+      <NewExpense />
       <Expenses />
     </div>
   );
