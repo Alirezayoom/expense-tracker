ce676957298b82b6b0700a648981eb238867567a
alirezayoom <alirezayoom@gmail.com>
2023-02-19 15:36:00 +0430

    expenses items added to app and mapped in expenses component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import Expenses from "./components/Expenses/Expenses";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const expenses = [
+  {
+    id: 1,
+    title: "iPhone 14 pro",
+    amount: "999",
+    date: new Date(2023, 1, 25),
+  },
+  {
+    id: 2,
+    title: "Galaxy S23 FE",
+    amount: "599",
+    date: new Date(2023, 1, 29),
+  },
+];
+
 function App() {
   return (
     <div>
       <NewExpense />
-      <Expenses />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,36 +2,18 @@ import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
-export default function Expenses() {
-  const expenses = [
-    {
-      id: 1,
-      title: "iPhone 14 pro",
-      amount: "999",
-      date: new Date(2023, 1, 25),
-    },
-    {
-      id: 2,
-      title: "Galaxy S23 FE",
-      amount: "599",
-      date: new Date(2023, 1, 29),
-    },
-  ];
-
+export default function Expenses({ items }) {
   return (
     <Card className="expenses">
-      <ExpenseItem
-        id={expenses[0].id}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        id={expenses[1].id}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
+      {items.map((item) => (
+        <div key={item.id}>
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        </div>
+      ))}
     </Card>
   );
 }
